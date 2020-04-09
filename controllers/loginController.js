10221e934dc9220f3be2680cef1933f723710a49
Matthew <mattghanno@gmail.com>
2020-04-09 17:31:13 +1000

    Installed mysql package and made a connection to the mysql server

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,9 +1,19 @@
 const showLoginPage = (app) => {
-    app.get("/", (req, res)=>{
+    app.get("/", (req, res) => {
         res.sendFile("/index.html");
     });
 }
 
+const checkLoginDetails = (app) => {
+    app.post("/", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        
+        res.send("<script>alert('Successfully logged in')</script>");
+    });
+}
+
 module.exports = {
-    showLoginPage: showLoginPage
+    showLoginPage: showLoginPage,
+    checkLoginDetails: checkLoginDetails
 }
---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mySQL = require("mysql");
+
+const loginController = require("./controllers/loginController.js");
+
+const connection = mySQL.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "eHealthDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+    console.log("Successfully connected to DB");
+})
+
+const ehealthApp = express();
+
+ehealthApp.use(bodyParser.urlencoded({extended: true}));
+
+ehealthApp.listen(3000, () => {
+    console.log("Node server started on port 3000");
+});
+
+ehealthApp.use(express.static(__dirname + "/public"));
+
+loginController.showLoginPage(ehealthApp);
+loginController.checkLoginDetails(ehealthApp);
