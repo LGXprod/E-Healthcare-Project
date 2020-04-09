b411066a8fc9e69f7a8940bb3a27fefe1ff3daa0
Matthew <mattghanno@gmail.com>
2020-04-09 23:09:10 +1000

    Added the fields to get the data required for an instance of Patient in DB

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -6,7 +6,24 @@ const showRegisterPage = (app, dir) => {
 
 const registerUser = (app, connection) => {
     app.post("/registration", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
 
+        const queryString = "select username from Patient where username='" + username + "';"
+        // need to find a way to prevent sql injections
+
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            } else {
+                if (result.length == 0) {
+                    connection.query("insert into Patient (username, userPassword)")
+                    res.send("<script>alert('Your account has now been created'</script>");
+                } else {
+                    res.send("<script>alert('That username is unavailable'</script>");
+                }
+            }
+        });
     });
 }
 
