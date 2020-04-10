23d802cc720b20ff2786c0a5860a9145238e93ae
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-04-10 13:27:55 +1000

    LoginPage-Commit_1
    Login landing page + sign up here pop up window

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,6 @@
 const showLoginPage = (app, dir) => {
     app.get("/", (req, res) => {
-        res.sendFile(dir + "/index.html");
+        res.sendFile(dir + "/LoginPage.html");
     });
 }
 
@@ -8,7 +8,7 @@ const checkLoginDetails = (app, connection) => {
     app.post("/", (req, res) => {
         const username = req.body.username;
         const password = req.body.password;
-        
+
         var queryString = "select username from Patient where username='" + username + "' and userPassword='" + password + "';";
 
         connection.query(queryString, (err, result) => {
@@ -29,3 +29,40 @@ module.exports = {
     showLoginPage: showLoginPage,
     checkLoginDetails: checkLoginDetails
 }
+
+
+
+
+//Get all elements in index.html with class=".input"
+const inputs = document.querySelectorAll(".input");
+
+//If clicked on it will focus
+function addfocus(){
+let parent = this.parentNode.parentNode;
+parent.classList.add("focus");
+}
+//If clicked somewhere else it will focus somewhere else
+function remfocus(){
+let parent = this.parentNode.parentNode;
+if(this.value == ""){
+  parent.classList.remove("focus");
+}
+}
+
+inputs.forEach(input => {
+input.addEventListener("focus", addfocus);
+input.addEventListener("blur", remfocus);
+});
+
+// Opens Signup when Sign up here is clicked
+var links = document.getElementsByTagName('h1');
+for( var i=0,il = links.length; i< il; i ++ ){
+ links[i].onclick = function(){
+   document.querySelector(".signup").style.display = "flex";
+ }
+}
+
+// Close the signup window
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".signup").style.display = "none";
+});
