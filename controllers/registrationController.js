fba887669d5ea2d3986c75254f3f135568788fec
Matthew <mattghanno@gmail.com>
2020-04-12 16:36:58 +1000

    Split addNewPat() into isUsernameAvailable() and insertNewPatDB() for encapsulation and to more efficiently handle promises and async functions

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,3 +1,5 @@
+const patient = require("../models/patient");
+
 const showRegisterPage = (app, dir) => {
     app.get("/registration", (req, res) => {
         res.sendFile(dir + "/registration.html");
@@ -7,34 +9,19 @@ const showRegisterPage = (app, dir) => {
 const registerUser = (app, connection) => {
     app.post("/registration", (req, res) => {
         const body = req.body;
-        console.log(body);
         const username = body.username;
         const password = body.password;
 
-        const queryString = "select username from Patient where username='" + username + "';";
-        // need to find a way to prevent sql injections
+        patient.isUsernameAvailable(connection, username).then((isAvailable) => {
+            console.log(isAvailable);
 
-        connection.query(queryString, (err, result) => {
-            if (err) {
-                throw err;
+            if (isAvailable) {
+                console.log("Yes");
             } else {
-                if (result.length == 0) {
-                    const addNewPatQuery = "insert into Patient (username, userPassword, fName, sName, medicareNo, medicareIRN, medicareExpiry) values "
-                                            + "('" + username + "', '" + password + "', '" + body.fName + "', '" + body.sName + "', "
-                                            +  body.medicareNo + ", " + body.medicareIRN + ", '" + body.medicareExpiry + "');";
-                    console.log(addNewPatQuery);
-                    connection.query(addNewPatQuery, (err) => {
-                        if (err) {
-                            throw err;
-                        } else {
-                            console.log("New user added.")
-                        }
-                    });
-                    res.send("<script>alert('Your account has now been created'</script>");
-                } else {
-                    res.send("<script>alert('That username is unavailable'</script>");
-                }
+                console.log("No");
             }
+        }).catch((err) => {
+            console.log(err);
         });
     });
 }
--- models/patient.js
+++ models/patient.js
@@ -1,3 +1,38 @@
+async function isUsernameAvailable(connection, username) {
+    return new Promise((resolve, reject) => {
+        connection.query("select username from Patient where username='" + username + "';", (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (result.length == 0) { // result.length is the number of patient's with that username
+                    resolve(true);
+                } else {
+                    resolve(false);
+                }
+            }
+        });
+    });
+}
+
+async function insertNewPatDB(connection, body, username, password) {
+    return new Promise((resolve, reject) => {
+        const addNewPatQuery = "insert into Patient (username, userPassword, fName, sName, medicareNo, medicareIRN, medicareExpiry) values "
+        + "('" + username + "', '" + password + "', '" + body.fName + "', '" + body.sName + "', "
+        +  body.medicareNo + ", " + body.medicareIRN + ", '" + body.medicareExpiry + "');";
+
+        console.log(addNewPatQuery);
+
+        connection.query(addNewPatQuery, (err) => {
+            if (err) { // if there is an error with the insert query
+                reject(err);
+            } else {
+                resolve(true);
+            }
+            // if it doesn't meet the above condition it is assumed the insert query for the new patient is successful
+        });
+    });
+}
+
 const checkLoginCredentials = (connection, username, password) => {
     return new Promise((resolve, reject) => {
         var queryString = "select username from Patient where username='" + username + "' and userPassword='" + password + "';";
@@ -17,5 +52,7 @@ const checkLoginCredentials = (connection, username, password) => {
 }
 
 module.exports = {
-    checkLoginCredentials: checkLoginCredentials
+    checkLoginCredentials: checkLoginCredentials,
+    isUsernameAvailable: async (connection, username) => { return await isUsernameAvailable(connection, username) },
+    insertPatientIntoDB: async (connection, body, username, password) => { return await insertNewPatDB(connection, body, username, password) }
 }
