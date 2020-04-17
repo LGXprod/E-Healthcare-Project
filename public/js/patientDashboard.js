de06403531527538c37ed0f391b51aed8aa2a798
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-04-17 11:35:23 +1000

    Front end of patient edit profile

--- public/js/patientDashboard.js
+++ public/js/patientDashboard.js
@@ -10,3 +10,20 @@ $(function(){
 //    $("#sideMenu a").slice(0,1,2,3,4,5,6).css({"background": "#397298", "border-left": "5px solid #1cb100",
 //    "padding": "15px 15px 15px 25px", "width": "280px", "transition": "0s"});
 //});
+
+// Opens edit profile pop up when edit profile button here is clicked
+$(document).ready(function() {
+    $(".editBtn").click(function(){
+        document.querySelector(".edit-profile").style.display = "flex";
+    });
+});
+
+// Close the edit profile pop up
+document.querySelector(".close").addEventListener("click", function(){
+    document.querySelector(".edit-profile").style.display = "none";
+    //Clears inputs if the signup window is closed
+  //  var inputs = document.getElementsByClassName(".edit-profile");
+    //  for (var i=0 ; i < inputs.length ; i++){
+      //      inputs[i].value = "";
+        //  }
+});
