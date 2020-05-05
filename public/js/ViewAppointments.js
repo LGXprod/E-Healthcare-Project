3babc2b277d596ad2195032ac936cc07735b394f
MichaelLarsen28 <62678953+MichaelLarsen28@users.noreply.github.com>
2020-05-05 15:54:30 +1000

    doctors appointment page

--- public/js/ViewAppointments.js
+++ public/js/ViewAppointments.js
@@ -1,95 +1,105 @@
-//once the button is clicked for that specific date
-$("#appointments-btn").click(() => {
+$("#appointment-btn").click(() => {
     var [year, month, day] = $("#date").val().split("-");
     month = parseInt(month);
     const date = year + "-" + (month < 10 ? "0" + month : month) + "-" + day;
     console.log(date)
 
+    var refreshIntervalId = setInterval(appointmentInterval, 5000);
 
-// gets the json array, and loops through it and prints out the result/values
-var refreshIntervalId = setInterval(appointmentInterval, 5000) //refreshes the array every 6 secs from the server. 
+    function appointmentInterval(){
+        $.ajax({url: "/appointmentsAvailable?date=" + date, success: function(result) {
+            console.log(result);
+        }});
+    }
 
-          function appointmentInterval(){
-              $.ajax({url: "/appointmentsAvailable?date=" + date, success: function(result) {
-      $.each(result, function(i, val) {
-        console.log(result);
-      });
- }});
-}
+    document.querySelector(".av-table table").style.display = "table";
 
+    $(document).ready(function() {
+        // Fetch the initial table
+       refreshTable();
+    });
 
+    //needs to refresh table that is clicked on (this just refreshes the first initial clicked on table)
+    function refreshTable(){
+        $.ajax({url: "/appointmentsAvailable?date=" + date, success: function(result) {
+              var doctorData = '';
 
-// $.ajax({url: "/appointmentsAvailable?date=" + date, success: function(result) {
-//       $.each(result, function(i, val) {
-//         // if(result.username == document.getElementById("#docName").value)
-//            console.log(result);
-//       });
-//     }});
-
-// function myFunction() {
-//   document.getElementById("docName").innerHTML = .findIndex(/appointmentsAvailable);
-// }
-
-
-// get the initial table.
- $(document).ready(function() {
-                  // Fetch the initial table
-                 refreshTable();
+              var docUsername = document.getElementById("docName").innerHTML;
+              //find the index that matches the logged in doctors username to the JSON array
+              var index = result.findIndex(function(item, i){
+                return item.username === docUsername;
               });
 
+        for(var a = 0; a < Object.keys(result[index].schedule.appointments).length; a++) {
 
-function refreshTable(){
-                  $.ajax({url: "/appointmentsAvailable?date=" + date, success: function(result) {
-                        var doctorData = ''; 
+            $.each(result[index], function(item, val){
 
-                  //looping thorough the appointments array inside the JSON array.
-                  for(var a = 0; a < Object.keys(result[0].schedule.appointments).length; a++) {
+                var startTime = result[index].schedule.startTime;
+                var minsAfter = result[index].schedule.appointments[a].minutesAfterStart;
 
-                      //changes the minutes after to real life time i.e. 10:15am
-                      $.each(result[0], function(index, val){
 
-                          var startTime = result[0].schedule.startTime;
-                          var minsAfter = result[0].schedule.appointments[a].minutesAfterStart;
+                //change start time of 10:00 to "10"
+                var splitHM = startTime.split(":");
+                var hour = parseInt(splitHM[0]);
 
-                          //change start time of 10:00 to "10"
-                          var splitHM = startTime.split(":");
-                          var hour = parseInt(splitHM[0]);
+                var timeAfterStart = new Date();
 
-                          var timeAfterStart = new Date();
+                timeAfterStart.setHours(hour);
+                timeAfterStart.setMinutes(minsAfter);
 
-                          timeAfterStart.setHours(hour);
-                          timeAfterStart.setMinutes(minsAfter);
+                var appointmentTime = (timeAfterStart.getHours() + ":" + timeAfterStart.getMinutes());
+                //change the time to 12 hour with AM and PM
+                function formatTwelveHour(date) {
+                    var hours = timeAfterStart.getHours();
+                    var minutes = timeAfterStart.getMinutes();
+                    var ampm = hours >= 12 ? 'PM' : 'AM'; // assigns pm if over 12 and am if under 12
+                    hours = hours % 12;
+                    hours = hours ? hours : 12; // sets hour 0 to 12
+                    minutes = minutes < 10 ? '0'+ minutes : minutes;
+                    var strTime = hours + ':' + minutes + ' ' + ampm;
+                    return strTime;
+                }
+                // add the time to column 1
+                doctorData += "<tr><td>" + formatTwelveHour(appointmentTime) + "</td>";
+                // add available or unavailable to column 2
+                if(result[index].schedule.appointments[a].isAvailable) {
+                     doctorData += "<td>" + "Available" + "</td>";
+                }
+                else{
+                     doctorData += "<td>" + "Unavailable" + "</td>";
+                }
+                  return false;
+                });
+        }
+        $('#doctor-app table tbody').html(doctorData); //apend this all to doctor-app table
 
-                          var appointmentTime = (timeAfterStart.getHours() + ":" + timeAfterStart.getMinutes());
+        // delete row if available (we only want to show unavailable appointments)
+        $(function(){
+            $("#doctor-app tr").each(function(){
+              var col_val = $(this).find("td:eq(1)").text();
+              if (col_val == "Available"){
+                $(this).text('')
+                 }
+            });
+          });
+}});
 
-                          //function changes to am/pm instead of 24 hour time.
-                          function formatTwelveHour(date) {
-                              var hours = timeAfterStart.getHours();
-                              var minutes = timeAfterStart.getMinutes();
-                              var ampm = hours >= 12 ? 'PM' : 'AM'; // assigns pm if over 12 and am if under 12
-                              hours = hours % 12;
-                              hours = hours ? hours : 12; // sets hour 0 to 12
-                              minutes = minutes < 10 ? '0'+ minutes : minutes;
-                              var strTime = hours + ':' + minutes + ' ' + ampm;
-                              return strTime;
-                          }
-
-                          //puts values in the doctor data variable/table initialized at the top.
-                          doctorData += "<tr><td>" + formatTwelveHour(appointmentTime) + "</td>"; 
-
-                          //loops through the appointments to check which ones are available and adds a new coloumn for results.
-                          if(!result[0].schedule.appointments[a].isAvailable) {
-                               doctorData += "<td>" + "Unavailable" + "</td>";
-                              }
-                              
-                            return false;
-                          });
-                  }
-                  
-                  // add the results to the html/ejs table
-                  $('#doctorapp table tbody').html(doctorData);
-    }
-});
 }
 });
-                  
+
+
+$(function () {
+    $("#sideMenu a").bind("click", function () {
+        $("#sideMenu a").removeClass("clicked"); // Remove all highlights
+        $(this).addClass("clicked"); // Add the class only for actually clicked element
+    });
+});
+
+//Home highlighted when the PatientDashbaord page is loaded
+$(document).ready(function () {
+    $("#sideMenu a").slice(1, 2, 3, 4, 5, 6).css({
+        "background": "#397298", "border-left": "5px solid #aa74da",
+        "padding": "15px 15px 15px 25px", "width": "280px", "transition": "0s"
+    });
+    $("#sideMenu a i").slice(1, 2, 3, 4, 5, 6).css({ "color": "#aa74da" });
+});
