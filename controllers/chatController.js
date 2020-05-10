049fa254525f2ef3199663ec4d906182b3c324c4
LGXprod <mattghanno@gmail.com>
2020-05-10 16:35:35 +1000

    Changed href to use /Chat instead of /Chat/Patient or doctor because it will make the socket.io implementation easier

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -5,12 +5,30 @@ const showChatPage = (app, connection, io) => {
     app.get("/Chat", (req, res) => {
         const username = req.session.username;
 
-        doctor.getAllDoctors(connection, "*").then((doctors) => {
-            res.render("PatientChat", {
-                doctors: doctors
-            });
-        }).catch((err) => console.log(err));
+        if (req.query.id == null) {
+            doctor.getAllDoctors(connection, "*").then((doctors) => {
+                res.render("PatientChat", {
+                    doctors: doctors
+                });
+            }).catch((err) => console.log(err));
+        } else {
+
+        }
+        
     });
+
+    // app.get("/Chat", (req, res) => {
+    //     const doc_username = req.session.username;
+    //     const pat_username = req.query.username;
+
+    //     patient.isUsernameAvailable(connection, pat_username).then((isNotPatient) => {
+    //         if (!isNotPatient) { // if they are a registered patient
+
+    //         } else {
+
+    //         }
+    //     }).catch((err) => console.log(err));
+    // });
 
     io.on("connection", (socket) => {
         socket.on('chat message', (msg) => {
